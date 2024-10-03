ff5e612601a45c204652f8f4bbd6f4c45ed42f85
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-10-03 17:08:48 +0530

    Added IDs to projects and refactored the code

--- src/todo-item.js
+++ src/todo-item.js
@@ -3,26 +3,32 @@ import querySelectors from './query-selectors.js';
 // Array to store created todo item object
 const todoList = [];
 
+// Todo ID Counter
+let todoId = 1;
+
 // Todo Item Constructor Class
 class TodoItem {
-    constructor(title, description, dueDate, priority, notes, checklist) {
+    constructor(id, title, description, dueDate, priority, notes, projectId) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
         this.notes = notes;
-        this.checklist = checklist;
+        this.projectId = projectId;
     }
 }
 
 // Funtion to create new todo item object and add it to array
 function addNewTodo() {
     let todo = new TodoItem(
+        todoId++,
         querySelectors.todoTitle.value,
         querySelectors.todoDescription.value,
         querySelectors.dueDate.value,
         querySelectors.todoPriority.value,
-        querySelectors.todoNotes.value
+        querySelectors.todoNotes.value,
+        querySelectors.todoProject.value
     );
 
     todoList.push(todo);
@@ -30,14 +36,18 @@ function addNewTodo() {
 
 // Todo Database Logic
 function addTodoToDatabase() {
-    todoList.forEach((todo, index) => {
+    todoList.forEach(todo => {
         let todoBox = document.createElement('div');
         todoBox.classList.add('todoBox');
-        todoBox.innerHTML = `<h3>${todo.title}</h3><p>${
-            todo.description
-        }</p><p>${todo.dueDate}</p><p>${priorityCheck(todo.priority)}</p><p>${
-            todo.notes
-        }</p>`;
+        todoBox.innerHTML = `
+        <h3>${todo.title}</h3>
+        <p>${todo.id}</p>
+        <p>${todo.description}</p>
+        <p>${todo.dueDate}</p>
+        <p>${priorityCheck(todo.priority)}</p>
+        <p>${todo.notes}</p>
+        <p>Project ID: ${todo.projectId}</p>
+        `;
         querySelectors.todoSection.appendChild(todoBox);
     });
 }
