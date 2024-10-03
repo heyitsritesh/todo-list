fdf5646bbc5fb8f6cf712180eb0997bc3fe74462
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-10-03 14:56:38 +0530

    Moved new todo logic to this module

--- src/todo-item.js
+++ src/todo-item.js
@@ -1,4 +1,10 @@
-export default class TodoItem {
+import querySelectors from './query-selectors.js';
+
+// Array to store created todo item object
+const todoList = [];
+
+// Todo Item Constructor Class
+class TodoItem {
     constructor(title, description, dueDate, priority, notes, checklist) {
         this.title = title;
         this.description = description;
@@ -8,3 +14,49 @@ export default class TodoItem {
         this.checklist = checklist;
     }
 }
+
+// Funtion to create new todo item object and add it to array
+function addNewTodo() {
+    let todo = new TodoItem(
+        querySelectors.todoTitle.value,
+        querySelectors.todoDescription.value,
+        querySelectors.dueDate.value,
+        querySelectors.todoPriority.value,
+        querySelectors.todoNotes.value
+    );
+
+    todoList.push(todo);
+}
+
+// Todo Database Logic
+function addTodoToDatabase() {
+    todoList.forEach((todo, index) => {
+        let todoBox = document.createElement('div');
+        todoBox.classList.add('todoBox');
+        todoBox.innerHTML = `<h3>${todo.title}</h3><p>${
+            todo.description
+        }</p><p>${todo.dueDate}</p><p>${priorityCheck(todo.priority)}</p><p>${
+            todo.notes
+        }</p>`;
+        querySelectors.todoSection.appendChild(todoBox);
+    });
+}
+
+// Priority Text
+function priorityCheck(priority) {
+    if (priority === 'high') {
+        return querySelectors.highPriority;
+    } else if (priority === 'medium') {
+        return querySelectors.medPriority;
+    } else if (priority === 'low') {
+        return querySelectors.lowPriority;
+    }
+}
+
+// Rebuild Logic
+function rebuildTodo() {
+    querySelectors.todoSection.textContent = '';
+    addTodoToDatabase();
+}
+
+export { TodoItem, addTodoToDatabase, todoList, addNewTodo, rebuildTodo };
