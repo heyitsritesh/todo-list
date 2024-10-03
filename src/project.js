01156d4fa4450a8aa0af9247ef4fec52a972e2b9
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-10-03 17:04:45 +0530

    Several improvements:
    - Projects now have IDs
    - Default project is created by default
    - Project module now adds new projects to dropdown in todo form

--- src/project.js
+++ src/project.js
@@ -3,17 +3,32 @@ import querySelectors from './query-selectors.js';
 // Array to store created project object
 const projectList = [];
 
+// Project ID Counter
+let projectId = 1;
+
 // Project Constructor Class
 class Project {
-    constructor(name, description) {
+    constructor(id, name, description) {
+        this.id = id;
         this.name = name;
         this.description = description;
     }
 }
 
+// Default Project
+(function () {
+    const defaultProject = new Project(
+        0,
+        'Default Project',
+        'This is a default project and all the todos without a project will go here'
+    );
+    projectList.push(defaultProject);
+})();
+
 // Funtion to create new project object and add it to array
 function addNewProject() {
     let project = new Project(
+        projectId++,
         querySelectors.projectTitle.value,
         querySelectors.projectDescription.value
     );
@@ -23,12 +38,16 @@ function addNewProject() {
 
 // Project List Logic
 function addProjectToList() {
-    projectList.forEach((project, index) => {
+    for (let i = 1; i < projectList.length; i++) {
         let projectName = document.createElement('div');
         projectName.classList.add('projectName');
-        projectName.innerHTML = `<h3>${project.name}</h3><p>${project.description}</p>`;
+        projectName.innerHTML = `
+        <h3>${projectList[i].name}</h3>
+        <p>${projectList[i].id}</p>
+        <p>${projectList[i].description}</p>
+        `;
         querySelectors.projectsSection.appendChild(projectName);
-    });
+    }
 }
 
 // Rebuild Logic
@@ -37,4 +56,21 @@ function rebuildProject() {
     addProjectToList();
 }
 
-export { addProjectToList, addNewProject, rebuildProject };
+// Available Projects as Options Under Todo
+function availableProjects() {
+    querySelectors.todoProject.textContent = '';
+    for (let project of projectList) {
+        const selectObject = document.createElement('option');
+        selectObject.setAttribute('value', project.id);
+        selectObject.textContent = project.name;
+        querySelectors.todoProject.appendChild(selectObject);
+    }
+}
+
+export {
+    addProjectToList,
+    addNewProject,
+    rebuildProject,
+    availableProjects,
+    projectList,
+};
