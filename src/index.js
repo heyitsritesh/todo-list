db71b74286613985b387f92cf54a613478c285bc
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-10-04 17:06:40 +0530

    Added logic for 'all todos' button and added a hell lot of comments to improve readability

--- src/index.js
+++ src/index.js
@@ -1,39 +1,48 @@
 import './styles.css';
-import { addTodoToDatabase, addNewTodo, rebuildTodo } from './todo-item.js';
+import querySelectors from './query-selectors.js';
+import { addNewTodo, rebuildTodo, renderTodos } from './todo.js';
 import {
-    addProjectToList,
+    renderProjectsDatabase,
     addNewProject,
     rebuildProject,
     availableProjects,
-    projectList,
 } from './project.js';
 
+//// Initialize available projects ////
 availableProjects();
 
-// "Add New Todo" Button Logic
+//// Show default project ////
+renderProjectsDatabase();
+
+//// All Todos ////
+querySelectors.allTodos.addEventListener('click', () => {
+    rebuildTodo();
+});
+
+//// "Add New Todo" Button Logic ////
 addNewTodoBtn.addEventListener('click', () => {
     newTodoDialog.showModal();
 });
 
-// "Add New Project" Button Logic
+//// "Add New Project" Button Logic ////
 addNewProjectBtn.addEventListener('click', () => {
     newProjectDialog.showModal();
 });
 
-// New Todo Form Submission Logic
+//// New Todo Form Submission Logic ////
 addTodo.addEventListener('submit', event => {
-    event.preventDefault();
-    addNewTodo();
-    addTodoToDatabase();
-    availableProjects();
-    rebuildTodo();
+    event.preventDefault(); // Disables default form behaviour
+    addNewTodo(); // Adds todo item to database
+    renderTodos(); // Render todos from database
+    availableProjects(); // Syncs project dropdown with new projects
+    rebuildTodo(); // Re-render todos
 });
 
-// New Project Form Submission Logic
+//// New Project Form Submission Logic ////
 addProject.addEventListener('submit', event => {
-    event.preventDefault();
-    addNewProject();
-    addProjectToList();
-    availableProjects();
-    rebuildProject();
+    event.preventDefault(); // Disables default form behaviour
+    addNewProject(); // Adds new project to database
+    renderProjectsDatabase(); // Render projects from database
+    availableProjects(); // Syncs project dropdown with new projects
+    rebuildProject(); // Re-render projects
 });
