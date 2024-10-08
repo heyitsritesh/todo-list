710ac67e32b2d4b5d51c8cb42575e818812a98fb
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-10-08 14:51:31 +0530

    Deployment commit

--- src/active-tab.js
+++
@@ -1,6 +0,0 @@
-export default function activeTab(clickedTab) {
-    const tabs = [...document.querySelectorAll('.project, #allTodos')];
-    tabs.forEach(tab => tab.classList.remove('active'));
-
-    clickedTab.classList.add('active');
-}
